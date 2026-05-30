643e8236724bd6372e454dca54ee4e89106270d0
Kevin Pintar <kevinpintar@hotmail.fr>
2026-05-31 01:22:13 +0200

    chore: initialize Vue Router and add basic views

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { i18n } from "../i18n";
+import { i18n } from "./i18n/index.ts";
+import { createPinia } from "pinia";
+import router from "./router/index.ts";
 
+const pinia = createPinia();
 const app = createApp(App);
+app.use(router);
 app.use(i18n);
+app.use(pinia);
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { onMounted, watch } from "vue";
-import { useBookStore } from "./stores/bookStore";
+import { useBookStore } from "../stores/bookStore";
 
 const bookStore = useBookStore();
 onMounted(() => {
   bookStore.fetchBooks();
-  console.log("Initial books:", JSON.stringify(bookStore.books, null, 2));
 });
 
 watch(
@@ -15,5 +14,12 @@ watch(
 </script>
 
 <template>
-  <RouterView />
+  <div>
+    <h1>Home View fonctionne !</h1>
+    <ul>
+      <li v-for="book in bookStore.books" :key="book.id">
+        {{ book.title }} by {{ book.author }}
+      </li>
+    </ul>
+  </div>
 </template>
